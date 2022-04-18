4eb19c6635e895f7839dc48081717876d9824a2e
가은 <user@192.168.45.72>
2022-04-18 23:05:12 +0900

    ex09

---
+++ src/com/javaex/practice/Ex09.java
@@ -0,0 +1,16 @@
+package com.javaex.practice;
+
+public class Ex09 {
+
+	public static void main(String[] args) {
+		
+		for(int y = 0; y<=9; y++) {
+			for(int x = 1; x<=10; x++) {
+				System.out.print((x+y)+"\t");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
