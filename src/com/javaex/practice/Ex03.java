7dd3cff69fafb5d3bf939972b92dd43d79364841
가은 <user@192.168.45.72>
2022-04-18 17:39:29 +0900

    ex03

---
+++ src/com/javaex/practice/Ex03.java
@@ -0,0 +1,19 @@
+package com.javaex.practice;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		
+		int x, y;
+		
+		for(x = 0; x<4; x++) {
+			
+			for(y = 2; y>=0; y--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		} //***가 세로로 4줄 만들어질 것으로 예상
+
+	}
+
+}
