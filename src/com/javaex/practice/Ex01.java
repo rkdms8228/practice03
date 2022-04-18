14a5aa7a71145b80da8b863b7ef98dd0a69f3949
가은 <user@192.168.45.72>
2022-04-18 17:33:00 +0900

    ex01, ex02

--- src/com/javaex/practice/Ex01.java
+++ src/com/javaex/practice/Ex01.java
@@ -1,9 +1,22 @@
-package com.javaex.ex01;
+package com.javaex.practice;
 
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("while문");
+		int index = 0;
+		
+		while(index<3) {
+			System.out.println("index="+index);
+			index++;
+		} //index=2까지 반복될 것으로 예상
+		
+		System.out.println("for문");
+		
+		for(int order = 0; order<3; order++) {
+			System.out.println("order="+order);
+		} //order=2까지 반복될 것으로 예상
 
 	}
 
---
+++ src/com/javaex/practice/Ex02.java
@@ -0,0 +1,19 @@
+package com.javaex.practice;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		
+		int x, y;
+		
+		for(x = 0; x<4; x++) {
+			
+			for(y = 0; y<2; y++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		} //**가 세로 4줄로 만들어질 것으로 예상
+		
+	}
+
+}
