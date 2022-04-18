f0288fc5414a2b83ad59da1beb972afbe6d76602
가은 <user@192.168.45.72>
2022-04-18 18:30:47 +0900

    ex08 tap 수정

--- src/com/javaex/practice/Ex08.java
+++ src/com/javaex/practice/Ex08.java
@@ -7,7 +7,7 @@ public class Ex08 {
 		for(int y = 1; y<=9; y++) {
 			for(int x = 2; x<=9; x++) {
 				int sum = x*y;
-				System.out.print(x+" * "+y+" = "+sum+"   ");
+				System.out.print(x+" * "+y+" = "+sum+"\t");
 			}
 			System.out.println();
 		}
