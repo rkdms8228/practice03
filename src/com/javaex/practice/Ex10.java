e88d63075c8ca1c6085fe5ba9763984fd93ee7ec
가은 <user@192.168.0.10>
2022-04-19 09:33:34 +0900

    ex10

---
+++ src/com/javaex/practice/Ex10.java
@@ -0,0 +1,32 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex10 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("숫자를 입력하세요.");
+			
+			Scanner sc = new Scanner(System.in);
+			int max = 0;
+			int num = 0;
+		
+			for(int i = 1; i<=5; i++) {
+				
+				System.out.print("숫자: ");
+				num = sc.nextInt();
+				
+					if(num>max) {
+						max = num;
+					}
+					
+			}
+			
+			System.out.println("최대값은 "+max+"입니다.");
+			
+			sc.close();		
+				
+	}
+
+}
