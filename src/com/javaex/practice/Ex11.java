7257f827c5c65f3c8d0b3aba24cf3bb3d42a5262
가은 <rkdms8228@naver.com>
2022-04-19 16:19:10 +0900

    ex11

---
+++ src/com/javaex/practice/Ex11.java
@@ -0,0 +1,30 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex11 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력하세요.");
+		int num = sc.nextInt();
+		
+		int sum = 0;
+		
+		for(int i = 1; i<=num; i++) {
+			if(num%2==0) {
+				int en;
+			}else {
+				int on;
+			}
+		}
+		
+		System.out.println("결과값: "+sum);
+		
+		sc.close();
+
+	}
+
+}
